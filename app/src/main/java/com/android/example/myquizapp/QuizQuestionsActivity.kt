34cd5c153701e4766c44aee411930fdfa195c6c1
sauna1137 <subscription.search@gmail.com>
2023-10-04 21:53:27 +0900

    add constants

--- app/src/main/java/com/android/example/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/android/example/myquizapp/QuizQuestionsActivity.kt
@@ -2,10 +2,14 @@ package com.android.example.myquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestion()
+        Log.i("question size ","${questionsList.size}")
     }
 }
