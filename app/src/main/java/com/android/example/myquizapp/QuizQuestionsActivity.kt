ceea7863fac120a3d284d7bc34f18a4c0ffd4b71
sauna1137 <subscription.search@gmail.com>
2023-10-07 11:11:18 +0900

    add variable to quizQuestion activity

--- app/src/main/java/com/android/example/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/android/example/myquizapp/QuizQuestionsActivity.kt
@@ -3,8 +3,16 @@ package com.android.example.myquizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
